6433b42b16c5d2a4bb65259de90a3068b9f25141
saram <saram9p@gmail.com>
2021-09-28 16:11:50 +0900

    router.py, news_api.py 완료

--- router.py
+++ router.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", "")
+    return render_template("index.html", newsList= na.getNewsApi())
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
